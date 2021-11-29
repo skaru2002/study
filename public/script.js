f49abfb36514649541087a95f6f7982d816b4505
skaru2000 <skaru2002@naver.com>
2021-11-29 16:49:33 +0900

    camera connect & room make & available many people in one room

---
+++ public/script.js
@@ -0,0 +1,59 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    Audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected',userID => {
+        connectToNewUser(userID, stream)
+    })
+})
+
+socket.on('user-disconnected', userID => {
+    if (peers[userID]) peers[userID].close()
+})
+
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userID, stream) {
+    const call = myPeer.call(userID,stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+
+    call.on('close', () => {
+        video.remove()
+    })
+    peers[userID] = call
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get ('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomID: req.params.room })
+})
+
+io.on('connection', (socket) => {    
+    socket.on('join-room', (roomID, userID) => {
+        socket.join(roomID)
+        socket.broadcast.to(roomID).emit('user-connected',userID)
+
+        socket.on('disconnect', () => {
+            socket.broadcast.to(roomID).emit('user-disconnected',userID)
+        })
+    })
+})
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
